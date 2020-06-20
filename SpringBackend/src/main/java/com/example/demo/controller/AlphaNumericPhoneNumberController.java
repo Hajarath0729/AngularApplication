21aa6514c091fd3a66fa6b4214ea14660be83318
Hajarathaiah Podalakuru <podalakurus@Hajarathaiahs-MacBook-Pro.local>
2020-06-20 00:21:51 -0400

    Adding Cross Origion Annotation

--- SpringBackend/src/main/java/com/example/demo/controller/AlphaNumericPhoneNumberController.java
+++ SpringBackend/src/main/java/com/example/demo/controller/AlphaNumericPhoneNumberController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class AlphaNumericPhoneNumberController {
 	@Autowired
 	private AlphaNumericPhoneNumberService alphaNumericPhoneNumberService;
 	
+	@CrossOrigin
 	@GetMapping(value="/alpha-numeric-phonenumbers")
 	public List<String> getAlphaNumericPhoneNumbers(@RequestParam String phoneNumber) {
 		return alphaNumericPhoneNumberService.getAlphaNumericPhoneNumbers(phoneNumber);
